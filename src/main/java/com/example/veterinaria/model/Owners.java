f54707ba6bdc1f8553853dda93febb41a0acf4ec
abelloch <abelloch.es@gmail.com>
2024-07-24 12:33:55 +0200

    model uptadate

--- src/main/java/com/example/veterinaria/model/Owners.java
+++ src/main/java/com/example/veterinaria/model/Owners.java
@@ -23,8 +23,49 @@ public class Owners {
     @Column(name = "email")
     private String email;
 
-    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "owner", cascade = CascadeType.ALL)
     private Set<Pet> pets = new HashSet<>();
 
     // Getters y Setters
+
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
--- src/main/java/com/example/veterinaria/model/Pet.java
+++ src/main/java/com/example/veterinaria/model/Pet.java
@@ -3,15 +3,16 @@ package com.example.veterinaria.model;
 import jakarta.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 @Entity
 @Table(name = "pets")
 public class Pet {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "pet_id")
-    private int petId;
+    private UUID uuid;
 
     @Column(name = "name")
     private String name;
@@ -25,13 +26,67 @@ public class Pet {
     @Column(name = "gender")
     private String gender;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "owner_id")
     private Owners owner;
 
     @OneToMany(mappedBy = "pet", cascade = CascadeType.ALL)
     private Set<AppointmentDetails> appointmentDetails = new HashSet<>();
 
-    // Getters y Setters
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Owners getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owners owner) {
+        this.owner = owner;
+    }
+
+    public Set<AppointmentDetails> getAppointmentDetails() {
+        return appointmentDetails;
+    }
+
+    public void setAppointmentDetails(Set<AppointmentDetails> appointmentDetails) {
+        this.appointmentDetails = appointmentDetails;
+    }
 }
 
